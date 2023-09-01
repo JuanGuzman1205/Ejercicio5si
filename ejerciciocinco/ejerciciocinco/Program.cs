c10413516e5c353029b4588bf9c305b1d06ce891
JuanGuzman1205 <jr20075jr@gmail.com>
2023-09-01 17:29:21 -0500

    Terminado

--- ejerciciocinco/ejerciciocinco/Program.cs
+++ ejerciciocinco/ejerciciocinco/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Dados los catetos de un triángulo rectángulo, calcular su hipotenusa.*/
+
+public class Ejercicio5
+{
+    static void Main(string[] args)
+    {
+        double catetoA, catetoB, hipotenusa;
+
+        Console.Write("Ingrese el valor de el cateto A: ");
+        catetoA = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Ingrese el valor del cateto B: ");
+        catetoB = Convert.ToDouble(Console.ReadLine());
+
+        hipotenusa = (Math.Sqrt((Math.Pow(catetoA, 2)) + (Math.Pow(catetoB, 2))));
+
+        Console.Write("El valor de la hipotenusa es:" +  hipotenusa);
+
+
+    }
+
+
+
+
+
+
+}
+
